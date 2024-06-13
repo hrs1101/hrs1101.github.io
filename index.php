31bcb826b190ed706d44b9850363943ed6ada66a
hrs1101 <46579711+hrs1101@users.noreply.github.com>
2024-06-13 10:58:57 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Membuat Kalkulator Tonase</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<?php 
+	if(isset($_POST['hitung'])){
+		$bil1 = $_POST['bil1'];
+		$bil2 = $_POST['bil2'];
+		$bil3 = $_POST['bil3'];
+		$bil4 = $_POST['bil4'];
+		$bil5 = $_POST['bil5'];
+		$bil6 = $_POST['bil6'];
+		$bil7 = $_POST['bil7'];
+		$bil8 = $_POST['bil8'];
+		$bil9 = $_POST['bil9'];
+		$bil10 = $_POST['bil10'];
+		$bil11 = $_POST['bil11'];
+        $a = $bil1*210*48/1000000;
+        $b = $bil2*200*48/1000000;
+        $c = $bil3*330*24/1000000;
+        $d = $bil4*600*24/1000000;
+        $e = $bil5*1500*12/1000000;
+        $f = $bil6*250*20/1000000;
+        $g = $bil7*330*12/1000000;
+        $h = $bil8*600*12/1000000;
+        $i = $bil9*1500*6/1000000;
+        $j = $bil10*19000/1000000;
+        $k = $bil11*18000/1000000;
+        $hasil = $a+$b+$c+$d+$e+$f+$g+$h+$i+$j+$k;
+		$output = number_format($hasil, 2, '.', '');
+		
+	}
+	?>
+	<div class="kalkulator">
+		<h2 class="judul">TONASE KALKULATOR</h2>
+		<a class="brand" href="">Created by Haris</a>
+		<form method="post" action="index.php">		
+            <br>
+            <br>
+            <br>
+
+			CUP 210ML : <input type="text" name="bil1" class="bil" autocomplete="off" value="0">
+            <br>
+			CUP 200ML : <input type="text" name="bil2" class="bil" autocomplete="off" value="0">
+            <br>
+			330ML 	  : <input type="text" name="bil3" class="bil" autocomplete="off" value="0">
+            <br>
+			600ML 	  : <input type="text" name="bil4" class="bil" autocomplete="off" value="0">
+            <br>
+			1500ML    : <input type="text" name="bil5" class="bil" autocomplete="off" value="0">
+            <br>
+            250ML WRP : <input type="text" name="bil6" class="bil" autocomplete="off" value="0">
+            <br>
+            330ML WRP : <input type="text" name="bil7" class="bil" autocomplete="off" value="0">
+            <br>
+			600ML WRP : <input type="text" name="bil8" class="bil" autocomplete="off" value="0">
+            <br>
+			1500ML WRP: <input type="text" name="bil9" class="bil" autocomplete="off" value="0">
+            <br>
+			GALON 19L : <input type="text" name="bil10" class="bil" autocomplete="off" value="0">
+            <br>
+			GALON 18L : <input type="text" name="bil11" class="bil" autocomplete="off" value="0">
+            <br>
+	
+			<input type="submit" name="hitung" value="Hitung" class="tombol">											
+		</form>
+		<?php if(isset($_POST['hitung'])){ ?>
+			<input type="text" value="<?php echo $output; ?>" class="bil">
+		<?php }else{ ?>
+			<input type="text" value="0" class="bil">
+		<?php } ?>			
+	</div>
+</body>
+</html>
